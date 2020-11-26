ce2c9b8b3cb4d4dbb6521e2efdbb4618001e9e5c
Victor Abrell <victor@abrell.se>
2020-11-26 17:38:10 +0100

    Inkludera autoloading

---
+++ wp-content/plugins/wc-invoice-payment-gateway/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8acf4ca64c8dac638090cedf4cb3497db1d38a5f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8acf4ca64c8dac638090cedf4cb3497db1d38a5f',
+    ),
+  ),
+);
