02e3f6e5e7c11c1fa35e91caa5ed501b2671a200
Victor Abrell <victor@abrell.se>
2020-11-27 09:25:32 +0100

    Dubbelkolla så WooCommerce är installerat

--- wp-content/plugins/wc-invoice-payment-gateway/src/InvoiceGateway.php
+++ wp-content/plugins/wc-invoice-payment-gateway/src/InvoiceGateway.php
@@ -1,126 +1,129 @@
 <?php
 defined("ABSPATH") || exit;
 
-class InvoiceGateway extends WC_Payment_Gateway {
+// Verify that WooCommerce is installed
+if (class_exists("WooCommerce")) {
+	class InvoiceGateway extends WC_Payment_Gateway {
 
-	public function __construct() {
-		$this->id = "ipg_invoice"; // payment gateway plugin ID
-		$this->icon = ""; // URL of the icon
-		$this->has_fields = true; // Custom form
-		$this->method_title = __("Invoice", "ipg");
-		$this->method_description = __("Use the Invoice payment method", "ipg"); // will be displayed on the options page
+		public function __construct() {
+			$this->id = "ipg_invoice"; // payment gateway plugin ID
+			$this->icon = ""; // URL of the icon
+			$this->has_fields = true; // Custom form
+			$this->method_title = __("Invoice", "ipg");
+			$this->method_description = __("Use the Invoice payment method", "ipg"); // will be displayed on the options page
+		
+			// Support
+			$this->supports = array(
+				"products"
+			);
 	
-		// Support
-		$this->supports = array(
-			"products"
-		);
-
-		// Load the options
-		$this->init_settings();
-		$this->title = $this->get_option("title");
-		$this->description = $this->get_option("description");
-		$this->enabled = $this->get_option("enabled");
-
-		// Method with all the options fields
-		$this->init_form_fields();
-
-		// This action hook saves the settings
-		add_action("woocommerce_update_options_payment_gateways_" . $this->id, array( $this,"process_admin_options"));
-	}
-
-	/**
-	 * Gateway options
-	 */
-	public function init_form_fields(){
-		$this->form_fields = array(
-			"enabled" => array(
-				"title"       => __("Enable/Disable", "ipg"),
-				"label"       => __("Enable Invoice Gateway", "ipg"),
-				"type"        => "checkbox",
-				"description" => "",
-				"default"     => "no"
-			),
-			"title" => array(
-				"title"       => __("Title", "ipg"),
-				"type"        => "text",
-				"description" => __("This controls the title which the user sees during checkout", "ipg"),
-				"default"     => "Invoice",
-				"desc_tip"    => true,
-			),
-			"description" => array(
-				"title"       => __("Description", "ipg"),
-				"type"        => "textarea",
-				"description" => __("This controls the description which the user sees during checkout", "ipg"),
-				"default"     => __("Pay with invoice", "ipg"),
-			),
-		);
-	}
-
-	/**
-	 * Custom form
-	 */
-	public function payment_fields() {
-		// Display the description if it's set
-		if ($this->description) {
-			// Display the description with <p> tags etc.
-			echo wpautop(wp_kses_post($this->description));
+			// Load the options
+			$this->init_settings();
+			$this->title = $this->get_option("title");
+			$this->description = $this->get_option("description");
+			$this->enabled = $this->get_option("enabled");
+	
+			// Method with all the options fields
+			$this->init_form_fields();
+	
+			// This action hook saves the settings
+			add_action("woocommerce_update_options_payment_gateways_" . $this->id, array( $this,"process_admin_options"));
 		}
-	 
-		// Display the form
-		?>
-		<fieldset id="ipg-<?php echo esc_attr($this->id); ?>-ssn-form" class="ipg-ssn" style="background:transparent;">
-			<div class="form-row form-row-wide">
-				<label><?php _e("Social Security Number", "ipg"); ?> <span class="required">*</span></label>
-				<input id="ipg_pnr" name="social_security_number" type="text" autocomplete="off">
-			</div>
-		</fieldset>
-		<?php
-	}
-
-	/**
-	 * Fields validation
-	 */
-	public function validate_fields() {
-		// Extract variables from $_POST
-		extract($_POST);
-
-		if (empty($social_security_number)) {
-			wc_add_notice(__("Social security number must be filled in", "ipg"), "error");
-			return false;
+	
+		/**
+		 * Gateway options
+		 */
+		public function init_form_fields(){
+			$this->form_fields = array(
+				"enabled" => array(
+					"title"       => __("Enable/Disable", "ipg"),
+					"label"       => __("Enable Invoice Gateway", "ipg"),
+					"type"        => "checkbox",
+					"description" => "",
+					"default"     => "no"
+				),
+				"title" => array(
+					"title"       => __("Title", "ipg"),
+					"type"        => "text",
+					"description" => __("This controls the title which the user sees during checkout", "ipg"),
+					"default"     => "Invoice",
+					"desc_tip"    => true,
+				),
+				"description" => array(
+					"title"       => __("Description", "ipg"),
+					"type"        => "textarea",
+					"description" => __("This controls the description which the user sees during checkout", "ipg"),
+					"default"     => __("Pay with invoice", "ipg"),
+				),
+			);
 		}
-
-		$luhn = new Luhn;
-
-		if (!$luhn->checkSSN($social_security_number)) {
-			wc_add_notice(__("Social security number is not valid", "ipg"), "error");
-			return false;
+	
+		/**
+		 * Custom form
+		 */
+		public function payment_fields() {
+			// Display the description if it's set
+			if ($this->description) {
+				// Display the description with <p> tags etc.
+				echo wpautop(wp_kses_post($this->description));
+			}
+		 
+			// Display the form
+			?>
+			<fieldset id="ipg-<?php echo esc_attr($this->id); ?>-ssn-form" class="ipg-ssn" style="background:transparent;">
+				<div class="form-row form-row-wide">
+					<label><?php _e("Social Security Number", "ipg"); ?> <span class="required">*</span></label>
+					<input id="ipg_pnr" name="social_security_number" type="text" autocomplete="off">
+				</div>
+			</fieldset>
+			<?php
 		}
-
-	}
-
-	/**
-	 * We're processing the payments here
-	 */
-	public function process_payment($order_id) {
-		global $woocommerce;
-
-		// Get order details
-		$order = wc_get_order($order_id );
-
-		// Complete order
-		$order->payment_complete();
-		wc_reduce_stock_levels($order_id);
-
-		// Display notes to customer
-		$order->add_order_note(__("Thank you for your order!\nWe will process the order as fast as possible.", "ipg"), true );
-
-		// Empty cart
-		$woocommerce->cart->empty_cart();
-
-		// Redirect to the thank you page
-		return array(
-			'result' => 'success',
-			'redirect' => $this->get_return_url($order)
-		);
+	
+		/**
+		 * Fields validation
+		 */
+		public function validate_fields() {
+			// Extract variables from $_POST
+			extract($_POST);
+	
+			if (empty($social_security_number)) {
+				wc_add_notice(__("Social security number must be filled in", "ipg"), "error");
+				return false;
+			}
+	
+			$luhn = new Luhn;
+	
+			if (!$luhn->checkSSN($social_security_number)) {
+				wc_add_notice(__("Social security number is not valid", "ipg"), "error");
+				return false;
+			}
+	
+		}
+	
+		/**
+		 * We're processing the payments here
+		 */
+		public function process_payment($order_id) {
+			global $woocommerce;
+	
+			// Get order details
+			$order = wc_get_order($order_id );
+	
+			// Complete order
+			$order->payment_complete();
+			wc_reduce_stock_levels($order_id);
+	
+			// Display notes to customer
+			$order->add_order_note(__("Thank you for your order!\nWe will process the order as fast as possible.", "ipg"), true );
+	
+			// Empty cart
+			$woocommerce->cart->empty_cart();
+	
+			// Redirect to the thank you page
+			return array(
+				'result' => 'success',
+				'redirect' => $this->get_return_url($order)
+			);
+		}
 	}
 }
