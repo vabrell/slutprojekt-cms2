a5adba1531a72064a647272d87227e347afc93a7
David Kjellson <david.kjellson@gmail.com>
2020-12-02 13:06:31 +0100

    Add SKU plugin to gitignore

--- wp-content/themes/slutprojekt-cms2/Classes/Theme.php
+++ wp-content/themes/slutprojekt-cms2/Classes/Theme.php
@@ -1,22 +1,26 @@
 <?php
-defined("ABSPATH") || exit;
 
 namespace SPC2;
 
+defined("ABSPATH") || exit;
+
 use WC_Tax;
 
-class Theme {
+class Theme
+{
 	const THEME_NAME = "slutprojekt-cms2";
 	const OPTION_PREFIX = "theme_is_activated_";
 
-	public function __construct() {
+	public function __construct()
+	{
 		add_action("switch_theme", [$this, "deactivateTheme"]);
 	}
 
 	/**
 	 * Run deactivation of theme
 	 */
-	public function deactivateTheme() {
+	public function deactivateTheme()
+	{
 		// Check if the theme is activated
 		if (get_option(self::OPTION_PREFIX . self::THEME_NAME)) {
 			// Find all tax rates
@@ -33,7 +37,8 @@ class Theme {
 	/**
 	 * Run activation of theme
 	 */
-	static function activateTheme() {
+	static function activateTheme()
+	{
 		// Check if the theme hasn't been activated
 		if (!get_option(self::OPTION_PREFIX . self::THEME_NAME)) {
 			// Run tax activation
@@ -46,7 +51,8 @@ class Theme {
 	/**
 	 * Activate WooCommerce Tax and add tax rate
 	 */
-	static function activateWooCommerceTax() {
+	static function activateWooCommerceTax()
+	{
 		// Check if WooCommerce is activated
 		if (class_exists("WooCommerce")) {
 			// Activate tax calculation
@@ -54,7 +60,7 @@ class Theme {
 			update_option("woocommerce_prices_include_tax", "yes");
 			update_option("woocommerce_tax_display_cart", "incl");
 			update_option("woocommerce_tax_display_shop", "incl");
-			
+
 			// Insert Swedish tax rate
 			WC_Tax::_insert_tax_rate([
 				"tax_rate_country" => "SE",
