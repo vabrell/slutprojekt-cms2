536e3e58383ca7a0a85e15a576128f73fdf93dcf
Victor Abrell <victor@abrell.se>
2020-12-04 10:09:26 +0100

    Sök och blogg inlägg

---
+++ wp-content/themes/slutprojekt-cms2/searchform.php
@@ -0,0 +1,11 @@
+<form action="<?php echo site_url(); ?>" class="form-inline my-2 my-lg-0">
+	<input
+		class="form-control mr-sm-2"
+		type="search"
+		name="s"
+		placeholder="<?php _e("Search", "spc2"); ?>"
+		aria-label="Search"
+		value="<?php the_search_query(); ?>"
+	>
+	<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+</form>
--- wp-content/themes/slutprojekt-cms2/single.php
+++ wp-content/themes/slutprojekt-cms2/single.php
@@ -5,7 +5,18 @@ if (have_posts()) {
 	the_post();
 	$title = get_the_title();
 	echo "<h1>{$title}</h1>";
+	if (has_post_thumbnail()) {
+		echo "<div class='my-3'>";
+			the_post_thumbnail();
+		echo "</div>";
+	}
 	the_content();
+
+	if (!is_product()) {
+		echo "<div class='my-3 pr-3 text-muted'>";
+			echo __("by", "spc2") . " " . get_the_author_meta("display_name");
+		echo "</div>";
+	}
 }
 
 get_footer();
