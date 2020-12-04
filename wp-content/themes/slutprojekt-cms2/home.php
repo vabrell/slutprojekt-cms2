4c657bbd4b00da1f610d9f2301668834e0ed158e
Victor Abrell <victor@abrell.se>
2020-12-04 10:29:14 +0100

    Blogg index

---
+++ wp-content/themes/slutprojekt-cms2/home.php
@@ -0,0 +1,34 @@
+<?php
+get_header();
+?>
+
+<h2>Blogg</h2>
+
+<?php
+if (have_posts()) {
+	while (have_posts()) {
+		the_post();
+?>
+		<a class="search-item" href="<?php echo get_the_permalink(); ?>">
+			<div class="row align-items-center my-4 p-2">
+				<?php
+				if (has_post_thumbnail()) {
+				?>
+					<div class="col-2">
+						<?php the_post_thumbnail("shop_thumbnail"); ?>
+					</div>
+				<?php
+				} 
+				?>
+				<div class="col-8">
+					<?php the_title(); ?>
+				</div>
+			</div>
+		</a>
+		<?php
+	}
+} else {
+	echo __("No blog posts could be found.", "spc2");
+}
+
+get_footer();
