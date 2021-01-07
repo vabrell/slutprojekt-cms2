ceb9954349b9538d31c1292e1a0a5ac9197efa16
David Kjellson <david.kjellson@gmail.com>
2021-01-07 03:11:10 +0100

    Ändrat till archive eftersom det man tydligen måste lägga till via custom post types

---
+++ wp-content/themes/slutprojekt-cms2/archive-butiker.php
@@ -0,0 +1,47 @@
+<?php /* Template Name: Butiker */
+
+get_header();
+
+while (have_posts()) {
+  the_post();
+  echo '<h1>' . get_the_title() . '</h1>';
+
+  if (have_rows('karta')) {
+    while (have_rows('karta')) {
+      the_row(); ?>
+      <div class="mapdiv">
+        <p class="address"><?php the_sub_field('gata') ?> <?php the_sub_field('gatunummer') ?>, <?php the_sub_field('postnummer') ?>, <?php the_sub_field('stad'); ?></p>
+        <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
+          <iframe src="https://www.google.com/maps/embed/v1/place?key=<?php echo get_field('api-nyckel'); ?>&q=<?php the_sub_field('gata'); ?>+<?php the_sub_field('gatunummer'); ?>,+<?php the_sub_field('postnummer'); ?>+<?php the_sub_field('stad'); ?>" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+      </div>
+<?php
+    }
+  }
+}
+get_footer(); ?>
+
+<style>
+  .mapdiv {
+    margin-left: 20%;
+  }
+
+  .address {
+    margin-left: 25%;
+  }
+
+  .map-container {
+    overflow: hidden;
+    padding-bottom: 56.25%;
+    position: relative;
+    height: 0;
+  }
+
+  .map-container iframe {
+    left: 0;
+    top: 0;
+    height: 50%;
+    width: 70%;
+    position: absolute;
+  }
+</style>
