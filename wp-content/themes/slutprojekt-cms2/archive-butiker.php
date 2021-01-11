742b9aa1cddebe15d9130cf07521017bb2d7f498
David Kjellson <david.kjellson@gmail.com>
2021-01-11 01:08:33 +0100

    Snyggare design på archive-butiker (men inga länkar)

--- wp-content/themes/slutprojekt-cms2/archive-butiker.php
+++ wp-content/themes/slutprojekt-cms2/archive-butiker.php
@@ -4,26 +4,27 @@ get_header(); ?>
 
 <h1>Våra butiker</h1>
 
-<?php
-while (have_posts()) {
-  the_post();
+<table class="table">
+  <tbody>
+    <?php
+    while (have_posts()) {
+      the_post();
+      if (have_rows('karta')) {
+        while (have_rows('karta')) {
+          the_row(); ?>
+          <tr>
+            <td><?php the_sub_field('gata') ?> <?php the_sub_field('gatunummer') ?></td>
+            <td><?php the_sub_field('postnummer') ?></td>
+            <td><?php the_sub_field('stad'); ?></td>
+          </tr>
+    <?php
+        }
+      }
+    } ?>
+  </tbody>
+</table>
 
-  if (have_rows('karta')) {
-    while (have_rows('karta')) {
-      the_row(); ?>
-      <div class="mapdiv">
-        <a href="<?php the_permalink(); ?>">
-          <p class="address"><?php the_sub_field('gata') ?> <?php the_sub_field('gatunummer') ?>, <?php the_sub_field('postnummer') ?>, <?php the_sub_field('stad'); ?></p>
-        </a>
-        <div id="map-container-google-1" class="z-depth-1-half map-container">
-          <iframe src="https://www.google.com/maps/embed/v1/place?key=<?php echo get_field('api-nyckel'); ?>&q=<?php the_sub_field('gata'); ?>+<?php the_sub_field('gatunummer'); ?>,+<?php the_sub_field('postnummer'); ?>+<?php the_sub_field('stad'); ?>" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
-      </div>
-<?php
-    }
-  }
-}
-get_footer(); ?>
+<?php get_footer(); ?>
 
 <style>
   .mapdiv {
