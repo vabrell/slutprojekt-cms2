dcd732847ae3f835b8d6f352a4f3b15aa4646a46
David Kjellson <david.kjellson@gmail.com>
2021-01-13 14:36:15 +0100

    Klass Pickup påbörjad, men får inte upp någon rubrik

--- wp-content/plugins/pickup/pickup.php
+++ wp-content/plugins/pickup/pickup.php
@@ -6,3 +6,22 @@
  * Version: 1.0
  * Description: Option to pick up product at store
  **/
+
+class Pickup
+{
+  function pickupalt()
+  {
+    ob_start(); ?>
+    <h1>God dag</h1>
+<?php
+    $pickupcode = ob_get_clean();
+    return $pickupcode;
+  }
+
+  public function __construct()
+  {
+    add_action('woocommerce_checkout_before_customer_details', [$this, 'pickupalt']);
+  }
+}
+
+$pickup = new Pickup();
