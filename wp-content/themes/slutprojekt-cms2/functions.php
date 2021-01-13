93159ae1d4a116590cf37050d44760070c59135d
Edwin Berg <e.berg117@gmail.com>
2021-01-13 13:31:47 +0100

    added a carousel with pictures which u can choose via acf

--- wp-content/themes/slutprojekt-cms2/functions.php
+++ wp-content/themes/slutprojekt-cms2/functions.php
@@ -44,3 +44,84 @@ add_action('init', 'butiker');
  * Theme support
  */
 add_theme_support("menus");
+
+/**
+ * Bildspel
+ */
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_5fd9f2ab46a54',
+	'title' => 'bildspel',
+	'fields' => array(
+		array(
+			'key' => 'field_5fd9f2db0e41e',
+			'label' => 'Content',
+			'name' => 'content',
+			'type' => 'repeater',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'collapsed' => '',
+			'min' => 0,
+			'max' => 0,
+			'layout' => 'table',
+			'button_label' => 'Add slide',
+			'sub_fields' => array(
+				array(
+					'key' => 'field_5fda01a429f1b',
+					'label' => 'Image',
+					'name' => 'image',
+					'type' => 'relationship',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'post_type' => array(
+						0 => 'product',
+					),
+					'taxonomy' => '',
+					'filters' => array(
+						0 => 'search',
+						1 => 'post_type',
+						2 => 'taxonomy',
+					),
+					'elements' => '',
+					'min' => '',
+					'max' => '',
+					'return_format' => 'object',
+				),
+			),
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'page',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => array(
+		0 => 'the_content',
+	),
+	'active' => true,
+	'description' => '',
+));
+
+endif;
