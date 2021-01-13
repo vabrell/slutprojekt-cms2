d38080508d8f6407bcaf3966a82c011c70793e45
David Kjellson <david.kjellson@gmail.com>
2021-01-13 14:04:43 +0100

    Nu kan man lägga till titlar på butiker, så att slugen inte är automatiskt utkast

--- wp-content/themes/slutprojekt-cms2/functions.php
+++ wp-content/themes/slutprojekt-cms2/functions.php
@@ -18,25 +18,25 @@ $wooComHooks = new SPC2\WooCommerceHooks;
 
 function butiker()
 {
-  $labels = [
-    'name' => 'Butiker',
-    'singular_name' => 'Butik',
-    'menu_name' => 'Butiker',
-    'add_new_item' => 'Lägg till butik',
-    'add_new' => 'Lägg till',
-    'edit_item' => 'Redigera',
-    'update_item' => 'Uppdatera'
-  ];
-  $args = [
-    'labels' => 'butiker',
-    'labels' => $labels,
-    'supports' => [
-      'custom-fields'
-    ],
-    'public' => true,
-    'has_archive' => true,
-  ];
-  register_post_type('butiker', $args);
+	$labels = [
+		'name' => 'Butiker',
+		'singular_name' => 'Butik',
+		'menu_name' => 'Butiker',
+		'add_new_item' => 'Lägg till butik',
+		'add_new' => 'Lägg till',
+		'edit_item' => 'Redigera',
+		'update_item' => 'Uppdatera'
+	];
+	$args = [
+		'labels' => 'butiker',
+		'labels' => $labels,
+		'supports' => [
+			'custom-fields', 'title'
+		],
+		'public' => true,
+		'has_archive' => true,
+	];
+	register_post_type('butiker', $args);
 }
 add_action('init', 'butiker');
 
@@ -48,80 +48,80 @@ add_theme_support("menus");
 /**
  * Bildspel
  */
-if( function_exists('acf_add_local_field_group') ):
+if (function_exists('acf_add_local_field_group')) :
 
-acf_add_local_field_group(array(
-	'key' => 'group_5fd9f2ab46a54',
-	'title' => 'bildspel',
-	'fields' => array(
-		array(
-			'key' => 'field_5fd9f2db0e41e',
-			'label' => 'Content',
-			'name' => 'content',
-			'type' => 'repeater',
-			'instructions' => '',
-			'required' => 0,
-			'conditional_logic' => 0,
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'collapsed' => '',
-			'min' => 0,
-			'max' => 0,
-			'layout' => 'table',
-			'button_label' => 'Add slide',
-			'sub_fields' => array(
-				array(
-					'key' => 'field_5fda01a429f1b',
-					'label' => 'Image',
-					'name' => 'image',
-					'type' => 'relationship',
-					'instructions' => '',
-					'required' => 0,
-					'conditional_logic' => 0,
-					'wrapper' => array(
-						'width' => '',
-						'class' => '',
-						'id' => '',
+	acf_add_local_field_group(array(
+		'key' => 'group_5fd9f2ab46a54',
+		'title' => 'bildspel',
+		'fields' => array(
+			array(
+				'key' => 'field_5fd9f2db0e41e',
+				'label' => 'Content',
+				'name' => 'content',
+				'type' => 'repeater',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'collapsed' => '',
+				'min' => 0,
+				'max' => 0,
+				'layout' => 'table',
+				'button_label' => 'Add slide',
+				'sub_fields' => array(
+					array(
+						'key' => 'field_5fda01a429f1b',
+						'label' => 'Image',
+						'name' => 'image',
+						'type' => 'relationship',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'post_type' => array(
+							0 => 'product',
+						),
+						'taxonomy' => '',
+						'filters' => array(
+							0 => 'search',
+							1 => 'post_type',
+							2 => 'taxonomy',
+						),
+						'elements' => '',
+						'min' => '',
+						'max' => '',
+						'return_format' => 'object',
 					),
-					'post_type' => array(
-						0 => 'product',
-					),
-					'taxonomy' => '',
-					'filters' => array(
-						0 => 'search',
-						1 => 'post_type',
-						2 => 'taxonomy',
-					),
-					'elements' => '',
-					'min' => '',
-					'max' => '',
-					'return_format' => 'object',
 				),
 			),
 		),
-	),
-	'location' => array(
-		array(
+		'location' => array(
 			array(
-				'param' => 'post_type',
-				'operator' => '==',
-				'value' => 'page',
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'page',
+				),
 			),
 		),
-	),
-	'menu_order' => 0,
-	'position' => 'normal',
-	'style' => 'default',
-	'label_placement' => 'top',
-	'instruction_placement' => 'label',
-	'hide_on_screen' => array(
-		0 => 'the_content',
-	),
-	'active' => true,
-	'description' => '',
-));
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => array(
+			0 => 'the_content',
+		),
+		'active' => true,
+		'description' => '',
+	));
 
 endif;
